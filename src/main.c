3a269ac1a0c868bda6ea4445bdca2150d17d5402
Joan Lima <joanlima95@gmail.com>
2020-07-04 13:43:37 -0300

    Complete Program about threads

--- src/main.c
+++ src/main.c
@@ -7,10 +7,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <pthread.h>
 
 #define NUMTHREADS 4 
+#define MAX 64
+
+pthread_mutex_t chave; 
+
+int qntd_primos=0,numeros=0; // Variaveis auxiliares
+unsigned int numb[MAX]; //Vetor que armazena os números que serão analisados
 
 unsigned long int ver_primo(unsigned long int Num) //Essa função retorna o número 1 se o número é primo, e 0 caso contrário
 {
@@ -32,44 +37,54 @@ unsigned long int ver_primo(unsigned long int Num) //Essa função retorna o nú
             div++;
          } else
          {
-            //printf("O número %ld não é primo\n", Num);
             qntd_divisores = 0;      
          }
    } 
       return div;
 }
 
-void* funcao_thread(void *arg) 
+void* worker(void *arg) //Função thread onde é realizada as verificações dos primos
 {
   int* N = (int*)arg;
-  printf("Estou na thread %d!\n", *N);
-  printf("Saindo da thread %d!\n", *N);
+  int cond_inicial = (*N);
+
+   int m = cond_inicial;
+   while (m < numeros)
+   {
+      pthread_mutex_lock(&chave);
+      qntd_primos +=ver_primo(numb[m]);
+      pthread_mutex_unlock(&chave);
+      m+=NUMTHREADS;
+   }
 
   return NULL;
 }
 
-int main(int argc, char **argv) {
+int main() {
 
-  pthread_t threads[NUMTHREADS];
-  
-  void *args;
-  int narg = 5;
+  pthread_t threads[NUMTHREADS]; //Vetor com threads
+  char c;
+  int cont=0,args_threads[NUMTHREADS]; //Variaveis auxiliares
+   
+  //Laços que inicializam os vetores para não lerem lixo
+  for(cont = 0; cont<MAX;cont++) numb[cont] = '\0';
+  for(cont = 0; cont<NUMTHREADS;cont++) threads[cont] = '\0';
 
-  args = (void*) &narg;
-
-  for (int i = 0; i < NUMTHREADS; i++)
+  //Laço de leitura e armazenamento da entrada
+  while ((c!='\n') && (c!=EOF)) 
   {
-    pthread_create(&(threads[i]),NULL,funcao_thread,args);  
+    c = scanf("%u",&numb[numeros]);
+    numeros++;
+  } 
+  for (int i = 0; i < NUMTHREADS; i++) 
+  {
+     args_threads[i] = i;
+     pthread_create(&(threads[i]),NULL,worker,&(args_threads[i]));  
   }
-  
+
   //Essa função encerra todas as threads em execução.
   for (int i = 0; i < NUMTHREADS; i++) pthread_join(threads[i],NULL);     
 
-  
-  
-
-  scanf("%d\n",&narg);
-
-  printf("Terminando programa!\n");
+  printf("%d\n",qntd_primos);
   return 0;
 }
